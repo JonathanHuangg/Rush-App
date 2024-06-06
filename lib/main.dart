4be04ee9c27e130717442944ead5ccc51e2cf8d8
Jonathan Huang <jonathannhuang@gmail.com>
2024-06-06 09:57:04 -0400

    branches are scary

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,22 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:rush_app/widgets/admin/admin_settings_widget.dart';
-import 'screens/common/rusher_registration.dart';
-import 'widgets/admin/admin_settings_widget.dart';
 
-// Iphone 15 simulator
-// xcrun simctl boot 60382070-7787-4574-A898-672D761082C3
-// flutter run -d 60382070-7787-4574-A898-672D761082C3
+import 'screens/common/rusher_registration.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
+    return MaterialApp( 
+      title: 'demo app',
+      theme: ThemeData( 
+        primarySwatch: Colors.green,
       ),
-      home: const TextAndCheckBox(), // change this to change the screen
+
+      home: const DynamicFormPage()
     );
   }
 }
 
-
-class WelcomePage extends StatefulWidget {
-  const WelcomePage({super.key});
-  
-  @override 
-  WelcomePageState createState() => WelcomePageState();
-}
-
-class WelcomePageState extends State<WelcomePage> with SingleTickerProviderStateMixin {
-
-  double _buttonOpacity = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // this will start the fade in for the start button
-
-    Future.delayed(const Duration(seconds: 3), () {
-      setState(() {
-        _buttonOpacity = 1.0;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [AnimatedTextKit(
-          animatedTexts: [
-            WavyAnimatedText(
-              'Welcome to RushMe',
-              textStyle: const TextStyle (
-                fontSize: 32.0,
-                fontWeight: FontWeight.bold
-                ),
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 20.0),
-          AnimatedOpacity(
-            opacity: _buttonOpacity, 
-            duration: const Duration(seconds: 1),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const DynamicFormPage())
-                );
-              },
-              child: const Text('Start')
-            )
-          )
-        ]
-      ))
-      
-    );
-  }
-}
-
-
-// this is to just ensure everything is working correctly
-class BlankPage extends StatelessWidget {
-  const BlankPage({super.key});
-
-  @override 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container()
-    );
-  }
-}
